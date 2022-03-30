7ba1b7340ddd59ef6174627f5f6589bdc7868278
Saikat Kundu <kundusaikat32@gmail.com>
2022-03-30 14:07:41 +0530

    updated app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Main } from "./components/main/Main";
+import { Footer } from "./components/footer/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Guys</h1>
+    </div>
+  );
+}
+
+export default App;
