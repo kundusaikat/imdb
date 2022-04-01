a18195cae7a235435b3fd8a9cac9f99e4c80faf9
Sandeep Kumar Murmu <sandeep.16murmu@gmail.com>
2022-04-01 21:07:46 +0530

    few updates

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,10 @@
 import { Main } from "../main/Main";
 import { Footer } from "../footer/Footer";
+import {Header } from "../header/Header"
 export const Home = () => {
   return (
     <div>
+      <Header />
       <Main />
       <Footer />
     </div>
