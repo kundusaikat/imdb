67e3267b02e1c3a80859523b9f7c75a7aeae3c52
Sandeep Kumar Murmu <sandeep.16murmu@gmail.com>
2022-04-01 19:19:38 +0530

    updated header

---
+++ src/components/header/fragments/HeadBar.jsx
@@ -0,0 +1,80 @@
+import axios from "axios";
+import { useState } from "react";
+import { Debouns } from "./debouncing";
+
+export const HeadBar = ({ setToggle, image }) => {
+  const [final, setFinal] = useState([]);
+  const getData = (text) => {
+    const { value } = text.target;
+    if (value.length > 0) {
+      axios
+        .get(`https://omdbapi.com/?s=${value}&apikey=fdcffdb4&type=movie`)
+        .then((res) => {
+          console.log(res.data);
+          if (res.data.Response === "False") {
+            console.log("hi");
+            setFinal([]);
+          } else {
+            setFinal(res.data.Search);
+          }
+        });
+    } else if (value.length === 0) {
+      setFinal([]);
+    }
+  };
+  return (
+    <>
+      <div className="mainContainer">
+        <img src={image} alt="" className="logoYellow" />
+
+        <div className="menu">
+          <a href="" className="menuContainer">
+            <i
+              className="fa-solid fa-bars"
+              style={{ color: "white", background: "transparent" }}
+              onClick={(e) => {
+                e.preventDefault();
+                setToggle(false);
+              }}
+            >
+              <span></span> Menu
+            </i>
+          </a>
+        </div>
+        <div className="search">
+          <div className="searchOption">
+            <p id="movie">movies</p>
+          </div>
+          <input
+            type="text"
+            placeholder="Search.."
+            id="searchInput"
+            onChange={(e) => getData(e)}
+          />
+          <i className="fa-solid fa-magnifying-glass searchIcon"></i>
+        </div>
+
+        <div className="imdbBlack">
+          <p>ImdbPro</p>
+        </div>
+        <div className="imdbBlack">
+          <p>Watchlist</p>
+        </div>
+        <div className="imdbBlack">
+          <p>Sign in</p>
+        </div>
+        <div className="imdbBlack">
+          <select name="" id="imdbBlack_option">
+            <option value="">En</option>
+            <option value="">Hin</option>
+          </select>
+        </div>
+      </div>
+      <table className="debounce_table">
+        {final.map((e) => (
+          <Debouns key={e.imdbID} e={e} />
+        ))}
+      </table>
+    </>
+  );
+};
---
+++ src/components/header/header.jsx
@@ -0,0 +1,27 @@
+import image from "../../images/imdbYellow.png";
+import { Menu } from "./fragments/Menu";
+import { HeadBar } from "./fragments/HeadBar";
+import { After_head } from "./fragments/after_Header";
+
+import "./css/header.css";
+import "./css/menu.css";
+import { useState } from "react";
+
+export const Header = () => {
+  const [toggle, setToggle] = useState(true);
+  let arr = ["a", "you", "hi", "avanger"];
+  return (
+    <>
+      <div className="before_container">
+        {toggle ? (
+          <HeadBar setToggle={setToggle} image={image} />
+        ) : (
+          <Menu setToggle={setToggle} />
+        )}
+      </div>
+      <div className="after_container">
+        <After_head />
+      </div>
+    </>
+  );
+};
