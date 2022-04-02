1481204fa759ed089fc3e11dcf12b0692e51e361
Saikat Kundu <kundusaikat32@gmail.com>
2022-04-02 14:38:51 +0530

    update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,10 +2,12 @@ import { Main } from "../main/Main";
 
 import { Header } from "../header/Header";
 import { Footer } from "../footer/Footer";
+// import { Slider } from "../slider/Slider";
 export const Home = () => {
   return (
     <div>
       <Header />
+      {/* <Slider /> */}
       <Main />
       <Footer />
     </div>
