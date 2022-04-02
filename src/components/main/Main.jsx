4d937cdec6f35471dce662c46b8e914e45f9d6a3
Saikat Kundu <kundusaikat32@gmail.com>
2022-04-02 14:45:23 +0530

    update

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitch,
+  faTwitter,
+  faInstagram,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 import {
   topPicks,
   watchlistData,
