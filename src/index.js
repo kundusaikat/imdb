b18d7fb38b5673ab8970b91a45c64af647e16bd5
Sandeep Kumar Murmu <sandeep.16murmu@gmail.com>
2022-04-02 18:12:20 +0530

    s

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(container);
 
 const render = () => {
   root.render(
-    <React.StrictMode>
+    <React.StrictMode> 
       <BrowserRouter>
         <App />
       </BrowserRouter>
