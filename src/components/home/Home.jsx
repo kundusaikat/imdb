def591bcc0af959be9819d721158cf71c27a0210
Saikat Kundu <kundusaikat32@gmail.com>
2022-04-02 13:33:42 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,6 @@ import { Main } from "../main/Main";
 
 import { Header } from "../header/Header";
 import { Footer } from "../footer/Footer";
-import {Header } from "../header/Header"
 export const Home = () => {
   return (
     <div>
